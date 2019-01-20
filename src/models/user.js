b4014f1ca44529356aede2a62eb42ed6a6ce288d
Tuncay Kaplan <trkaplan@gmail.com>
2019-01-20 21:05:06 +0300

    Modeling problem (#84)
    * Modeling problem
    
    * fix "foreign key constraint" error #84

--- src/models/user.js
+++ src/models/user.js
@@ -91,6 +91,8 @@ module.exports = (sequelize: Sequelize, dataTypes: DataTypes) => {
   });
 
   User.associate = (models) => {
+    User.hasMany(models.UserEvent);
+    User.belongsToMany(models.Event, { through: models.UserEvent, foreignKey: 'uuid' });
     User.belongsToMany(models.Community, { as: 'communities', through: models.CommunityUser });
   };
 
