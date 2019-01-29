e2c3a8211d6cb7ca9addaf8b347ac07ca77a32d5
Selman Kahya <selmankahya@hotmail.com>
2019-01-28 21:43:41 -0800

    scripts

--- scripts/db/staging/seeders/20190103071856-chat-messages.js
+++ scripts/db/staging/seeders/20190103071856-chat-messages.js
@@ -2,7 +2,7 @@ import uuid from 'uuid/v4';
 
 const messages = [
   {
-    channel_uuid: '23ea1110-d6a1-11e8-9f8b-f2801f1b9fd1',
+    channel_uuid: '43e8e515-f99d-4c4f-b883-f1095a5ca222',
     uuid: uuid(),
     sender_uuid: 'e62d78b3-b16c-485d-81a4-49815d7d0655',
     text: 'hello!',
@@ -10,7 +10,7 @@ const messages = [
     updated_at: new Date(),
   },
   {
-    channel_uuid: '23ea1110-d6a1-11e8-9f8b-f2801f1b9fd1',
+    channel_uuid: '43e8e515-f99d-4c4f-b883-f1095a5ca222',
     uuid: uuid(),
     sender_uuid: '33467ba2-d69d-11e8-9f8b-f2801f1b9fd1',
     text: 'whats up',
